a98599221d2e57da7ff5475e93aacd16399a08ae
Braian <braiancoronelosmar@gmail.com>
2019-11-05 00:19:31 -0300

    First unit test

---
+++ app/src/main/java/org/itdevelopers/deportesunq/Team.kt
@@ -0,0 +1,6 @@
+package org.itdevelopers.deportesunq
+
+data class Team(
+    val id: Int,
+    val alias: String,
+    val fullName: String)
---
+++ app/src/test/java/org/itdevelopers/deportesunq/TeamTest.kt
@@ -0,0 +1,25 @@
+package org.itdevelopers.deportesunq
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import kotlin.Any as Any1
+
+class TeamTest {
+
+    @Test
+    fun getId() {
+        assertEquals(0, 0)
+    }
+
+    @Test
+    fun getAlias() {
+        assertEquals("UNQ", "UNQ")
+    }
+
+    @Test
+    fun getFullName() {
+        assertEquals("Universidad Nacional de Quilmes", "Universidad Nacional de Quilmes")
+    }
+
+}
