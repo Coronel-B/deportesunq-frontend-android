6d3f0f639e93f51adb014163ac00e9ff5f950777
Braian <bcoronel@fusap.com.ar>
2019-11-12 15:14:42 -0300

    New design of model

--- app/src/main/java/org/itdevelopers/deportesunq/model/Competitions.kt
+++ app/src/main/java/org/itdevelopers/deportesunq/model/Competitions.kt
@@ -3,16 +3,23 @@ package org.itdevelopers.deportesunq.model
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName;
 
-data class Competition(
+data class Competitions(
     @SerializedName("id") @Expose private val id: Int,
     @SerializedName("name") @Expose private val name: Int,
     @SerializedName("year") @Expose private val year: String,
-    @SerializedName("champion") @Expose private val champion: String,
-    @SerializedName("competition_detail") @Expose private val competitionDetail: List<CompetitionDetail>? = null)
+    @SerializedName("champion_team") @Expose private val championTeam: String,
+    @SerializedName("competition_details") @Expose private val competitionDetails: List<CompetitionDetail>? = null)
 
 data class CompetitionDetail(
+    @SerializedName("id") @Expose private val id: Int,
+    @SerializedName("competition_detail_items") @Expose private val competitionDetailItems: List<CompetitionDetailItem>) {
+}
+
+data class CompetitionDetailItem(
+    @SerializedName("id") @Expose private val id: Int,
     @SerializedName("position") @Expose private val position: Int,
-    @SerializedName("team") @Expose private val team: Team)
+    @SerializedName("team") @Expose private val team: Team
+)
 
 data class Team(
     @SerializedName("id") @Expose private val id: Int,
